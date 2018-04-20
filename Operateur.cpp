1b9f387546ec9f176298e18555c0ef09282a2fc7
AM275 <anthony.morgado@isen.yncrea.fr>
2018-04-20 11:53:50 +0200

    init operateur

--- Operateur.cpp
+++ Operateur.cpp
@@ -2,3 +2,33 @@
 // Created by isen on 20/04/18.
 //
 
+
+#include "Operateur.h"
+#include <iostream>
+
+using namespace std;
+
+Operateur::Operateur() {
+   
+}
+
+
+Operateur::Operateur(const Addition& orig) {
+}
+
+virtual void Operateur::Calculer(){
+    
+};
+
+virtual void Operateur::Afficher(){
+    
+};
+
+virtual void Operateur::Afficher_npi(){
+    
+};
+
+Operateur::~Operateur() {
+    
+}
+
