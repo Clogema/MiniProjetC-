f71bb0a23460c193a83386255f994fbae008e1d0
AM275 <anthony.morgado@isen.yncrea.fr>
2018-04-20 14:31:31 +0200

    modif soustraction

--- Expression.cpp
+++ Expression.cpp
@@ -2,7 +2,7 @@
 // Created by isen on 20/04/18.
 //
 
-
+/*
 #include "Expression.h"
 #include <iostream>
 
@@ -26,4 +26,9 @@ virtual void Expression::Afficher_npi(){
 };
 
 Expression::~Expression() {
+<<<<<<< HEAD
 }
+=======
+    
+}*/
+>>>>>>> ccac118c0d7f3e0a0b36ad96f85527849e4e55a4
--- Expression.h
+++ Expression.h
@@ -6,14 +6,22 @@
 #define PROJET_EXPRESSION_H
 
 
+#include "Addition.h"
+
 class Expression {
     
 public:
     Expression();
     Expression(const Addition& orig);
+<<<<<<< HEAD
     virtual void Afficher()=0;
     virtual void Afficher_npi()=0;
     virtual void Calculer()=0;
+=======
+    virtual void Afficher() = 0;
+    virtual void Afficher_npi() = 0;
+    virtual void Calculer() = 0;
+>>>>>>> ccac118c0d7f3e0a0b36ad96f85527849e4e55a4
     ~Expression();
     
 private:
