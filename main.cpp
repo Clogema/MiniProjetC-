e22a4eb12711e8fa7fcdceef7e9a6bb0d95a4514
AM275 <anthony.morgado@isen.yncrea.fr>
2018-04-20 11:10:24 +0200

    test main

--- main.cpp
+++ main.cpp
@@ -4,3 +4,27 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+/*
+#include <iostream>
+#include <string>
+#include "addition.h"
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+    int x, y;
+    cout << "A :";
+    cin >> x;
+    cout << "B :";
+    cin >> y;
+    
+    addition *resultat = new addition(x, y);
+    resultat-> ~addition(); 
+     
+
+    return 0;
+}
+*/
+
